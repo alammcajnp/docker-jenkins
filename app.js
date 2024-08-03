34294d8945d7821c9de36977be82cb4fc95b6e3f
alammcajnp <alamjnp12@gmail.com>
2024-08-03 19:26:28 +0530

    change code

--- app.js
+++ app.js
@@ -7,17 +7,17 @@ route.use(bodyParser.json());
 route.get("/", async (req, res) => {
   res
     .status(200)
-    .json({ message: "This is route url, after deploy success1111" });
+    .json({ message: "This is route url, after deploy success, by ec2" });
 });
 
 route.get("/final", async (req, res) => {
-  res.status(200).json({ message: "This is final testing, Now Done " });
+  res.status(200).json({ message: "This is final testing, Now Done, by ec2 " });
 });
 
 route.get("/health", async (req, res) => {
-  res
-    .status(200)
-    .json({ message: "This is health check, after deploy success 111 333" });
+  res.status(200).json({
+    message: "This is health check, after deploy success 111 333, by ec2"
+  });
 });
 
 route.listen(3003, () => {
